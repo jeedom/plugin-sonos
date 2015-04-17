a0e8158cde9c7b5e316830c91d569127e60254c7
loic <lgevrey@oxya.com>
2015-04-17 17:05:30 +0200

    Update doc

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -26,6 +26,7 @@ function sonos3_install() {
 		$cron->setFunction('pull');
 		$cron->setEnable(1);
 		$cron->setDeamon(1);
+		$cron->setDeamonSleepTime(3);
 		$cron->setSchedule('* * * * *');
 		$cron->save();
 	}
@@ -41,6 +42,7 @@ function sonos3_update() {
 		$cron->setFunction('pull');
 		$cron->setEnable(1);
 		$cron->setDeamon(1);
+		$cron->setDeamonSleepTime(3);
 		$cron->setSchedule('* * * * *');
 		$cron->save();
 	}
