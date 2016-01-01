f88a9500b8d724e35a18facee988767e2dc920b6
loic <lgevrey@oxya.com>
2016-01-01 16:17:04 +0100

    Fix right

--- core/ajax/sonos3.ajax.php
+++ core/ajax/sonos3.ajax.php
@@ -20,7 +20,7 @@ try {
 	require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 	include_file('core', 'authentification', 'php');
 
-	if (!isConnect('admin')) {
+	if (!isConnect()) {
 		throw new Exception(__('401 - Accès non autorisé', __FILE__));
 	}
 
