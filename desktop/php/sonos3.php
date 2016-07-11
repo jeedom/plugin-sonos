01e1a0b7f4818a4fce7e5fc271ec8b10fc90fd90
Loïc <loic@jeedom.com>
2016-07-11 19:35:17 +0200

    Remove bootstrapswtich

--- desktop/php/sonos3.php
+++ desktop/php/sonos3.php
@@ -7,35 +7,35 @@ $eqLogics = eqLogic::byType('sonos3');
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un template}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
+  <div class="col-lg-2 col-md-3 col-sm-4">
+    <div class="bs-sidebar">
+      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un template}}</a>
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
-           </ul>
-       </div>
+     </ul>
    </div>
+ </div>
 
-   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-    <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-         <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-    </div>
-    <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-        <center>
-          <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
-      </center>
-      <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+ <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+  <div class="eqLogicThumbnailContainer">
+    <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+     <center>
+       <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
+     </center>
+     <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+   </div>
+   <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+    <center>
+      <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
   </div>
 </div>
 <div class="eqLogicThumbnailContainer">
@@ -59,86 +59,86 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-   <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-   <div class="row">
-    <div class="col-xs-6">
-        <form class="form-horizontal">
-            <fieldset>
+ <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+ <div class="row">
+  <div class="col-xs-6">
+    <form class="form-horizontal">
+      <fieldset>
 
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Nom du sonos}}</label>
-                    <div class="col-sm-5">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                    <div class="col-sm-5">
-                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                            <option value="">{{Aucun}}</option>
-                            <?php
+        <div class="form-group">
+          <label class="col-sm-3 control-label">{{Nom du sonos}}</label>
+          <div class="col-sm-5">
+            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+          <div class="col-sm-5">
+            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+              <option value="">{{Aucun}}</option>
+              <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                       </select>
-                   </div>
-               </div>
-               <div class="form-group">
-                <label class="col-sm-3 control-label" ></label>
-                <div class="col-sm-9">
-                 <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-                 <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
-             </div>
+           </select>
          </div>
-         <div class="form-group">
-            <label class="col-sm-3 control-label">{{Model}}</label>
-            <div class="col-sm-5">
-                <select type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="model" >
-                    <option value="PLAY1">Sonos Play 1</option>
-                    <option value="PLAY3">Sonos Play 3</option>
-                    <option value="PLAY5">Sonos Play 5</option>
-                    <option value="CONNECT">Sonos Connect</option>
-                    <option value="CONNECT:AMP">Sonos Connect AMP</option>
-                    <option value="PLAYBAR">Sonos Playbar</option>
-                </select>
-            </div>
+       </div>
+       <div class="form-group">
+        <label class="col-sm-3 control-label"></label>
+        <div class="col-sm-9">
+          <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+          <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
         </div>
-        <div class="form-group">
-            <label class="col-sm-3 control-label">{{IP}}</label>
-            <div class="col-sm-5">
-                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="logicalId" placeholder="IP"/>
-            </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-3 control-label">{{Model}}</label>
+        <div class="col-sm-5">
+          <select type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="model" >
+            <option value="PLAY1">Sonos Play 1</option>
+            <option value="PLAY3">Sonos Play 3</option>
+            <option value="PLAY5">Sonos Play 5</option>
+            <option value="CONNECT">Sonos Connect</option>
+            <option value="CONNECT:AMP">Sonos Connect AMP</option>
+            <option value="PLAYBAR">Sonos Playbar</option>
+          </select>
         </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-3 control-label">{{IP}}</label>
+        <div class="col-sm-5">
+          <input type="text" class="eqLogicAttr configuration form-control" data-l1key="logicalId" placeholder="IP"/>
+        </div>
+      </div>
     </fieldset>
-</form>
+  </form>
 </div>
 <div class="col-xs-6">
-    <center>
-        <img src="plugins/sonos3/doc/images/sonos3_icon.png" id="img_sonosModel" style="height : 300px;" />
-    </center>
+  <center>
+    <img src="plugins/sonos3/doc/images/sonos3_icon.png" id="img_sonosModel" style="height : 300px;" />
+  </center>
 </div>
 </div>
 
 <legend>{{Commande}}</legend>
 <table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th>{{Nom}}</th><th>{{Action}}</th>
-        </tr>
-    </thead>
-    <tbody>
-    </tbody>
+  <thead>
+    <tr>
+      <th>{{Nom}}</th><th>{{Action}}</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
 </table>
 
 <form class="form-horizontal">
-    <fieldset>
-        <div class="form-actions">
-            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-        </div>
-    </fieldset>
+  <fieldset>
+    <div class="form-actions">
+      <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+      <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+    </div>
+  </fieldset>
 </form>
 
 </div>
