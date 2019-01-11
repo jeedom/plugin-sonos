cb747684ca7aa947174d9119ae1ea0dad147379d
Loïc <loic@jeedom.com>
2019-01-11 10:54:04 +0100

    test

--- vendor/duncan3dc/sonos/src/Devices/Factory.php
+++ vendor/duncan3dc/sonos/src/Devices/Factory.php
@@ -3,7 +3,6 @@
 namespace duncan3dc\Sonos\Devices;
 
 use duncan3dc\Cache\ArrayPool;
-use duncan3dc\Log\LoggerAwareTrait;
 use duncan3dc\Sonos\Interfaces\Devices\DeviceInterface;
 use duncan3dc\Sonos\Interfaces\Devices\FactoryInterface;
 use Psr\Log\LoggerInterface;
@@ -12,48 +11,46 @@ use Psr\SimpleCache\CacheInterface;
 
 final class Factory implements FactoryInterface
 {
-  
-  use LoggerAwareTrait;
-  /**
-  * @var CacheInterface $cache The cache object to use for finding Sonos devices on the network.
-  */
-  private $cache;
-  
-  /**
-  * @var LoggerInterface $logger The logging object.
-  */
-  private $logger;
-  
-  
-  /**
-  * Create a new instance.
-  *
-  * @param CacheInterface $cache The cache object to use for finding Sonos devices on the network
-  * @param LoggerInterface $logger A logging object
-  */
-  public function __construct(CacheInterface $cache = null, LoggerInterface $logger = null)
-  {
-    if ($cache === null) {
-      $cache = new ArrayPool();
+    /**
+     * @var CacheInterface $cache The cache object to use for finding Sonos devices on the network.
+     */
+    private $cache;
+
+    /**
+     * @var LoggerInterface $logger The logging object.
+     */
+    private $logger;
+
+
+    /**
+     * Create a new instance.
+     *
+     * @param CacheInterface $cache The cache object to use for finding Sonos devices on the network
+     * @param LoggerInterface $logger A logging object
+     */
+    public function __construct(CacheInterface $cache = null, LoggerInterface $logger = null)
+    {
+        if ($cache === null) {
+            $cache = new ArrayPool();
+        }
+        $this->cache = $cache;
+
+        if ($logger === null) {
+            $logger = new NullLogger();
+        }
+        $this->logger = $logger;
     }
-    $this->cache = $cache;
-    
-    if ($logger !== null) {
-      $this->setLogger($logger);
+
+
+    /**
+     * Create a new device.
+     *
+     * @param string $ip The IP address of the device
+     *
+     * @return DeviceInterface
+     */
+    public function create($ip): DeviceInterface
+    {
+        return new Device($ip, $this->cache, $this->logger);
     }
-    $this->logger = $logger;
-  }
-  
-  
-  /**
-  * Create a new device.
-  *
-  * @param string $ip The IP address of the device
-  *
-  * @return DeviceInterface
-  */
-  public function create($ip): DeviceInterface
-  {
-    return new Device($ip, $this->cache, $this->logger);
-  }
 }
--- vendor/duncan3dc/sonos/src/Interfaces/Devices/FactoryInterface.php
+++ vendor/duncan3dc/sonos/src/Interfaces/Devices/FactoryInterface.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace duncan3dc\Sonos\Interfaces\Devices;
-use Psr\Log\LoggerAwareInterface;
 
-interface FactoryInterface extends LoggerAwareInterface
+interface FactoryInterface
 {
-  /**
-  * Create a new device.
-  *
-  * @param string $ip The IP address of the device
-  *
-  * @return DeviceInterface
-  */
-  public function create($ip): DeviceInterface;
+    /**
+     * Create a new device.
+     *
+     * @param string $ip The IP address of the device
+     *
+     * @return DeviceInterface
+     */
+    public function create($ip): DeviceInterface;
 }
