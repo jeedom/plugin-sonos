229a2479d623dccbfd4aadc53660a325642dff9c
Loïc <loic@jeedom.com>
2019-01-14 15:43:30 +0100

    Cleaning

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -20,44 +20,40 @@ $parentNumber = array();
 ?>
 
 <div class="row row-overflow">
-    <?php
-if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1 && init('report') != 1) {
-	echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
-} else {
-	echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
-}
-?>
-    <div class="bs-sidebar">
-        <ul id="ul_object" class="nav nav-list bs-sidenav">
-            <li class="nav-header">{{Liste objets}} </li>
-            <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-            <?php
-
-foreach ($allObject as $object_li) {
-	if ($object_li->getIsVisible() != 1 || count($object_li->getEqLogic(true, false, 'sonos3', null, true)) == 0) {
-		continue;
-	}
-	$margin = 5 * $object_li->getConfiguration('parentNumber');
-	if ($object_li->getId() == $object->getId()) {
-		echo '<li class="cursor li_object active" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=sonos3&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+	<?php
+	if (init('report') != 1) {
+		echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
 	} else {
-		echo '<li class="cursor li_object" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=sonos3&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+		echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
 	}
-}
-?>
-        </ul>
-    </div>
+	?>
+	<div class="bs-sidebar">
+		<ul id="ul_object" class="nav nav-list bs-sidenav">
+			<li class="nav-header">{{Liste objets}} </li>
+			<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+			<?php
+			
+			foreach ($allObject as $object_li) {
+				if ($object_li->getIsVisible() != 1 || count($object_li->getEqLogic(true, false, 'sonos3', null, true)) == 0) {
+					continue;
+				}
+				$margin = 5 * $object_li->getConfiguration('parentNumber');
+				if ($object_li->getId() == $object->getId()) {
+					echo '<li class="cursor li_object active" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=sonos3&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+				} else {
+					echo '<li class="cursor li_object" ><a data-object_id="' . $object_li->getId() . '" href="index.php?v=d&p=panel&m=sonos3&object_id=' . $object_li->getId() . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+				}
+			}
+			?>
+		</ul>
+	</div>
 </div>
 <?php
-if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1 && init('report') != 1) {
+if (init('report') != 1) {
 	echo '<div class="col-lg-10 col-md-9 col-sm-8" id="div_displayObject">';
 } else {
 	echo '<div class="col-lg-12 col-md-12 col-sm-12" id="div_displayObject">';
 }
-?>
-<i class='fa fa-picture-o cursor pull-left reportModeHidden' id='bt_displayObject' data-display='<?php echo $_SESSION['user']->getOptions('displayObjetByDefault') ?>' title="Afficher/Masquer les objets"></i>
-<br/>
-<?php
 echo '<div class="div_displayEquipement" style="width: 100%;">';
 if (init('object_id') == '') {
 	foreach ($allObject as $object) {
