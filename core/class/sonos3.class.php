dafc5a09b6dd3e94f6426ea700e10d053646906a
Loïc <loic@jeedom.com>
2019-10-09 11:48:58 +0200

    add domniante color 2

--- core/class/sonos3.class.php
+++ core/class/sonos3.class.php
@@ -335,6 +335,7 @@ class sonos3 extends eqLogic {
 					if ($eqLogic->checkAndUpdateCmd('track_image', $track->getAlbumArt())) {
 						file_put_contents(dirname(__FILE__) . '/../../../../plugins/sonos3/sonos_' . $eqLogic->getId() . '.jpg', file_get_contents($track->getAlbumArt()));
 						$eqLogic->checkAndUpdateCmd('dominantColor', getDominantColor(dirname(__FILE__) . '/../../../../plugins/sonos3/sonos_' . $eqLogic->getId() . '.jpg'));
+						eqLogic->checkAndUpdateCmd('dominantColor2', getDominantColor(dirname(__FILE__) . '/../../../../plugins/sonos3/sonos_' . $eqLogic->getId() . '.jpg',2));
 						$changed = true;
 					}
 				} else if (file_exists(dirname(__FILE__) . '/../../../../plugins/sonos3/sonos_' . $eqLogic->getId() . '.jpg')) {
@@ -816,6 +817,18 @@ class sonos3 extends eqLogic {
 			$dominantColor = new sonos3Cmd();
 			$dominantColor->setLogicalId('dominantColor');
 			$dominantColor->setName(__('Couleur dominante', __FILE__));
+		}
+		$dominantColor->setType('info');
+		$dominantColor->setSubType('string');
+		$dominantColor->setEqLogic_id($this->getId());
+		$dominantColor->save();
+		
+		
+		$dominantColor2 = $this->getCmd(null, 'dominantColor2');
+		if (!is_object($dominantColor2)) {
+			$dominantColor = new sonos3Cmd();
+			$dominantColor->setLogicalId('dominantColor2');
+			$dominantColor->setName(__('Couleur dominante 2', __FILE__));
 		}
 		$dominantColor->setType('info');
 		$dominantColor->setSubType('string');
